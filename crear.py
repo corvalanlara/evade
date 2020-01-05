c3a72127abbd679b3ccdf7d5430b2929be57297e
Daniel Corvalan <corvalanlara@protonmail.com>
2020-01-05 01:17:39 -0300

    con descripción correcta

--- crear.py
+++ crear.py
@@ -14,9 +14,9 @@ for tercia in grupos:
         if elemento is tercia[0]:
             html += f'<div class="columns">\n'
         if elemento in grupos[0]:
-            html += f'\t<div class="column">\n\t\t<figure class="image is-3by5">\n\t\t\t<img class="lazyload" src="images/{elemento}">\n\t\t</figure>\n\t</div>\n'
+            html += f'\t<div class="column">\n\t\t<figure class="image is-3by5">\n\t\t\t<img src="images/{elemento}">\n\t\t</figure>\n\t</div>\n'
         else:
-            html += f'\t<div class="elemento">\n\t\t<figure class="image is-3by5">\n\t\t\t<img class="lazyload" data-src="images/{elemento}" loading="lazy">\n\t\t</figure>\n\t</div>\n'
+            html += f'\t<div class="column">\n\t\t<figure class="image is-3by5">\n\t\t\t<img class="lazyload" data-src="images/{elemento}" loading="lazy">\n\t\t</figure>\n\t</div>\n'
         if elemento is tercia[-1]:  
             html += '</div>\n'
 
